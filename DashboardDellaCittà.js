bab4515a19ee5aa5407d67aa51773b10b3783958
Orsouene <orsoueneelaouizeb@gmail.com>
2025-03-17 01:15:40 +0100

    Bonus-2

--- DashboardDellaCittà.js
+++ DashboardDellaCittà.js
@@ -16,21 +16,49 @@ async function getDashboardData(query) {
     // console.log("city oggetto", cityName);
     // console.log("Aeroporto oggetto", aeroporto);
     const promise = [cityName, meteo, aeroporto];
-    const [destinazione, weathers, airoports] = await Promise.all(promise);
-    // console.log([destinazione, weathers, airoports]);
-    // console.log(weathers[0]);
-    return {
-      city: destinazione[0] ? destinazione[0].name : null,
-      country: destinazione[0] ? destinazione[0].country : null,
-      temperature: weathers[0] ? weathers[0].temperature : null,
-      weather: weathers[0] ? weathers[0].weather_description : null,
-      airport: airoports[0] ? airoports[0].name : null,
-    };
+    const [destinazioneRes, weathersRes, airoportsRes] =
+      await Promise.allSettled(promise);
+
+    console.log([destinazioneRes, weathersRes, airoportsRes]);
+    // console.log(weathersRes[0]);
+    let data = {};
+
+    //! DESTINAZIONE
+    if (destinazioneRes.status === "rejected") {
+      console.log("invalid destination  errore  :", destinazioneRes.status);
+      data.city = null;
+      data.country = null;
+    } else {
+      const destinazione = destinazioneRes.value[0];
+      data.city = destinazione ? destinazione.name : null;
+      data.country = destinazione ? destinazione.country : null;
+    }
+    //! WEATHER
+    if (weathersRes.status === "rejected") {
+      console.log("invalid destination  errore  :", weathersRes.status);
+      data.temperature = null;
+      data.weather_description = null;
+    } else {
+      const weather = weathersRes.value[0];
+      data.temperature = weather ? weather.temperature : null;
+      data.weather = weather ? weather.weather_description : null;
+    }
+    //! AEROPORT
+    if (airoportsRes.status === "rejected") {
+      console.log("invalid destination  errore  :", destinazioneRes.status);
+      data.airport = null;
+    } else {
+      const airoport = airoportsRes.value[0];
+      data.airport = airoport ? airoport.name : null;
+    }
+
+    console.log(data);
+    return data;
   } catch (error) {
     throw new Error(`non trova la query : ${query} ${"=>"} ${error.message}`);
   }
 }
-getDashboardData("vienna")
+getDashboardData("london")
   .then((data) => {
     // console.log("Dasboard data:", data);
     if (data.city != null && data.country != null) {
